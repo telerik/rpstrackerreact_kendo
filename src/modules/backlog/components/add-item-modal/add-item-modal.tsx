d37f2e487db1f3ba29a47f41366d95c373ed5990
Kiril Peyanski <peyanski@progress.com>
2022-11-15 16:04:00 +0200

    WIP: use KendoReact Dialog Component

--- src/modules/backlog/components/add-item-modal/add-item-modal.tsx
+++ src/modules/backlog/components/add-item-modal/add-item-modal.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import { Typography } from '@progress/kendo-react-common';
 import { Button } from '@progress/kendo-react-buttons';
 import { FormElement } from '@progress/kendo-react-form';
 import { Label } from '@progress/kendo-react-labels';
 import { Input, TextArea } from '@progress/kendo-react-inputs';
 import { DropDownList } from '@progress/kendo-react-dropdowns';
-import { PopupPropsContext } from '@progress/kendo-react-popup';
-import { Modal, ModalBody, ModalFooter } from "reactstrap";
+import { Dialog, DialogActionsBar } from '@progress/kendo-react-dialogs';
 import { ItemType } from "../../../../core/constants";
 import { EMPTY_STRING } from "../../../../core/helpers";
 import { PtItem } from "../../../../core/models/domain";
@@ -47,42 +45,32 @@ export function AddItemModal(props: AddItemModalProps) {
         setNewItem(initModalNewItem());
     }
 
-    return (
-        <Modal isOpen={modalShowing}>
-            <div className="modal-header">
-                <Typography.h4 className="modal-title" id="modal-basic-title">Add New Item</Typography.h4>
-                <Button fillMode={"flat"} type="button" onClick={()=>setShowModal(false)} aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </Button>
-            </div>
-            <ModalBody>
-                <FormElement horizontal={true}>
-                    <div className="k-form-field">
-                        <Label>Title</Label>
-                        <div className="k-form-field-wrap">
-                            <Input defaultValue={newItem.title} onChange={(e) => onFieldChange(e, 'title')} name="title" />
-                        </div>
+    return modalShowing ? (
+        <Dialog title="Add New Item" onClose={()=>setShowModal(false)}>
+            <FormElement horizontal={true}>
+                <div className="k-form-field">
+                    <Label>Title</Label>
+                    <div className="k-form-field-wrap">
+                        <Input defaultValue={newItem.title} onChange={(e) => onFieldChange(e, 'title')} name="title" />
                     </div>
-                    <div className="k-form-field">
-                        <Label>Description</Label>
-                        <div className="k-form-field-wrap">
-                            <TextArea rows={2} defaultValue={newItem.description} onChange={(e) => onFieldChange(e, 'description')} name="description" />
-                        </div>
+                </div>
+                <div className="k-form-field">
+                    <Label>Description</Label>
+                    <div className="k-form-field-wrap">
+                        <TextArea rows={2} defaultValue={newItem.description} onChange={(e) => onFieldChange(e, 'description')} name="description" />
                     </div>
-                    <div className="k-form-field">
-                        <Label>Type</Label>
-                        <div className="k-form-field-wrap">
-                            <PopupPropsContext.Provider value={(p) => ({...p, style: { ...(p.style || {}), zIndex: 1130 }})} >
-                                <DropDownList defaultValue={newItem.typeStr} data={itemTypesProvider} name="typeStr" onChange={(e) => onFieldChange(e, 'typeStr')}/>
-                            </PopupPropsContext.Provider>
-                        </div>
+                </div>
+                <div className="k-form-field">
+                    <Label>Type</Label>
+                    <div className="k-form-field-wrap">
+                        <DropDownList defaultValue={newItem.typeStr} data={itemTypesProvider} name="typeStr" onChange={(e) => onFieldChange(e, 'typeStr')}/>
                     </div>
-                </FormElement>
-            </ModalBody >
-            <ModalFooter>
+                </div>
+            </FormElement>
+            <DialogActionsBar>
                 <Button themeColor="secondary" onClick={()=>setShowModal(false)}>Cancel</Button>
                 <Button themeColor="primary" onClick={onAddSave}>Save</Button>{' '}
-            </ModalFooter>
-        </Modal >
-    );
+            </DialogActionsBar>
+        </Dialog >)
+        : null;
 }
--- src/modules/backlog/components/assignee-list-modal/assignee-list-modal.tsx
+++ src/modules/backlog/components/assignee-list-modal/assignee-list-modal.tsx
@@ -1,8 +1,6 @@
-import { Modal, ModalBody, ModalFooter } from "reactstrap";
-import { Typography } from "@progress/kendo-react-common";
+import { Dialog } from '@progress/kendo-react-dialogs';
 import { PtItem, PtUser } from "../../../../core/models/domain";
 import { PtNewItem } from "../../../../shared/models/dto/pt-new-item";
-import { Button } from "@progress/kendo-react-buttons";
 
 export type AssigneeListModalProps = {
     modalIsShowing: boolean;
@@ -16,31 +14,22 @@ export function AssigneeListModal(props: AssigneeListModalProps) {
 
     const { modalIsShowing, setModalIsShowing, users, selectAssignee } = props;
 
-    return (
-        <Modal isOpen={modalIsShowing}>
-            <div className="modal-header">
-                <Typography.h4 className="modal-title" id="modal-basic-title">Select Assignee</Typography.h4>
-                <Button fillMode={"flat"} type="button" onClick={() => setModalIsShowing(false)} aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </Button>
-            </div>
-            <ModalBody>
-                <ul className="list-group list-group-flush">
-                    {
-                        users.map((u: PtUser) => {
-                            return (
-                                <li key={u.id} className="list-group-item d-flex justify-content-between align-items-center" onClick={() => selectAssignee(u)}>
-                                    <span>{u.fullName}</span>
-                                    <span className="badge ">
-                                        <img src={u.avatar} className="li-avatar rounded mx-auto d-block" />
-                                    </span>
-                                </li>
-                            );
-                        })
-                    }
-                </ul>
-            </ModalBody>
-            <ModalFooter />
-        </Modal>
-    );
+    return modalIsShowing ? (
+        <Dialog title="Select Assignee" onClose={() => setModalIsShowing(false)} width={400} height={600}>
+            <ul className="list-group list-group-flush">
+                {
+                    users.map((u: PtUser) => {
+                        return (
+                            <li key={u.id} className="list-group-item d-flex justify-content-between align-items-center" onClick={() => selectAssignee(u)}>
+                                <span>{u.fullName}</span>
+                                <span className="badge ">
+                                    <img src={u.avatar} className="li-avatar rounded mx-auto d-block" />
+                                </span>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </Dialog>
+    ) : null;
 }
