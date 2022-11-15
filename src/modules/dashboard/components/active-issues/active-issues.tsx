bbea209847585768680406f34aa516f340b221a9
Kiril Peyanski <peyanski@progress.com>
2022-11-15 14:20:56 +0200

    WIP: fix chart padding

--- src/modules/dashboard/components/active-issues/active-issues.tsx
+++ src/modules/dashboard/components/active-issues/active-issues.tsx
@@ -1,65 +1,63 @@
 import React from "react";
 import { StatusCounts } from "../../models";
-import './active-issues.css';
+import "./active-issues.css";
 
 interface WelcomeProps {
-    statusCounts: StatusCounts
+  statusCounts: StatusCounts;
 }
 
 export function ActiveIssuesComponent(props: WelcomeProps) {
-    if (!props.statusCounts) {
-        return (
-            <div className="card">
-                <h3 className="card-header">Active Issues</h3>
-                <div className="card-block">
-                </div>
-            </div >
-        );
-    }
-
+  if (!props.statusCounts) {
     return (
-        <div className="row">
+      <div className="card">
+        <h3 className="card-header">Active Issues</h3>
+        <div className="card-block"></div>
+      </div>
+    );
+  }
 
-            <div className="col-12 col-lg-6 col-xl pb-4 active-issues">
-                <span className="comp-label">
-                    <strong>{props.statusCounts.activeItemsCount}</strong>
-                    <small>Active issues</small>
-                </span>
-            </div>
-
-            <div className="col-12 col-lg-6 col-xl pb-4 text-success closed-issues">
-                <span className="comp-label">
-                    <strong>{props.statusCounts.closedItemsCount}</strong>
-                    <small>Closed issues</small>
-                </span>
-            </div>
-
-            <div className="col-12 col-lg-6 col-xl pb-4 text-danger open-issues">
-                <span className="comp-label">
-                    <strong>{props.statusCounts.openItemsCount}</strong>
-                    <small>Open issues</small>
-                </span>
-            </div>
-
-            <div className="col-12 col-lg-6 col-xl pb-4 close-rate">
-                <span className="comp-label">
-                    <strong>{Intl.NumberFormat('en-US', { maximumSignificantDigits: 4 }).format(props.statusCounts.closeRate)}%</strong>
-                    <small>Close rate</small>
-                </span>
-                <p className="m-0 small text-uppercase text-muted">
-                    Highest:
-                    100%
-                    on Oct 11, 2018
-    </p>
-                <p className="m-0 small text-uppercase text-muted">
-                    Lowest:
-                    20%
-                    on Oct 9, 2018
-    </p>
-
-            </div>
-
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-lg-6 col-xl pb-4 active-issues">
+          <span className="comp-label">
+            <strong>{props.statusCounts.activeItemsCount}</strong>
+            <small>Active issues</small>
+          </span>
         </div>
 
-    );
-};
+        <div className="col-12 col-lg-6 col-xl pb-4 text-success closed-issues">
+          <span className="comp-label">
+            <strong>{props.statusCounts.closedItemsCount}</strong>
+            <small>Closed issues</small>
+          </span>
+        </div>
+
+        <div className="col-12 col-lg-6 col-xl pb-4 text-danger open-issues">
+          <span className="comp-label">
+            <strong>{props.statusCounts.openItemsCount}</strong>
+            <small>Open issues</small>
+          </span>
+        </div>
+
+        <div className="col-12 col-lg-6 col-xl pb-4 close-rate">
+          <span className="comp-label">
+            <strong>
+              {Intl.NumberFormat("en-US", {
+                maximumSignificantDigits: 4,
+              }).format(props.statusCounts.closeRate)}
+              %
+            </strong>
+            <small>Close rate</small>
+          </span>
+          <p className="m-0 small text-uppercase text-muted">
+            Highest: 100% on Oct 11, 2018
+          </p>
+          <p className="m-0 small text-uppercase text-muted">
+            Lowest: 20% on Oct 9, 2018
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/modules/dashboard/pages/dashboard/dashboard-page.tsx
+++ src/modules/dashboard/pages/dashboard/dashboard-page.tsx
@@ -2,168 +2,196 @@ import { cloneElement, useContext, useState } from "react";
 import { useQueries } from "react-query";
 import { Observable } from "rxjs";
 
-import { DashboardFilter, FilteredIssues } from "../../repositories/dashboard.repository";
+import {
+  DashboardFilter,
+  FilteredIssues,
+} from "../../repositories/dashboard.repository";
 import { formatDateEnUs } from "../../../../core/helpers/date-utils";
 import { ActiveIssuesComponent } from "../../components/active-issues/active-issues";
 import { StatusCounts } from "../../models";
 import { PtUser } from "../../../../core/models/domain";
-import { PtDashboardServiceContext, PtStoreContext, PtUserServiceContext } from "../../../../App";
+import {
+  PtDashboardServiceContext,
+  PtStoreContext,
+  PtUserServiceContext,
+} from "../../../../App";
 import { DashboardChart } from "../../components/active-issues/dashboard-chart";
 
-import { Button, ButtonGroup } from '@progress/kendo-react-buttons';
-import { ComboBox, ComboBoxChangeEvent } from '@progress/kendo-react-dropdowns';
-
+import { Button, ButtonGroup } from "@progress/kendo-react-buttons";
+import { ComboBox, ComboBoxChangeEvent } from "@progress/kendo-react-dropdowns";
 
 type DateRange = {
-    dateStart: Date;
-    dateEnd: Date;
+  dateStart: Date;
+  dateEnd: Date;
 };
 
 export function DashboardPage() {
-    const store = useContext(PtStoreContext);
-    const userService = useContext(PtUserServiceContext);
-    const dashboardService = useContext(PtDashboardServiceContext);
+  const store = useContext(PtStoreContext);
+  const userService = useContext(PtUserServiceContext);
+  const dashboardService = useContext(PtDashboardServiceContext);
 
-    const [filter, setFilter] = useState<DashboardFilter>({});
+  const [filter, setFilter] = useState<DashboardFilter>({});
 
-    const users$: Observable<PtUser[]> = store.select<PtUser[]>('users');
-    const [users, setUsers] = useState<PtUser[]>([]);
+  const users$: Observable<PtUser[]> = store.select<PtUser[]>("users");
+  const [users, setUsers] = useState<PtUser[]>([]);
 
-    function getQueryKey(keybase: string) {
-        return [keybase, filter];
-    }
+  function getQueryKey(keybase: string) {
+    return [keybase, filter];
+  }
 
-    const useDashboardData = (filter: DashboardFilter) => {
-        return useQueries<[StatusCounts, FilteredIssues]>([
-            {
-                queryKey: getQueryKey('items'),
-                queryFn: () => dashboardService.getStatusCounts(filter)
-            },
-            {
-                queryKey: getQueryKey('issues'),
-                queryFn: () => dashboardService.getFilteredIssues(filter)
-            }
-        ]);
+  const useDashboardData = (filter: DashboardFilter) => {
+    return useQueries<[StatusCounts, FilteredIssues]>([
+      {
+        queryKey: getQueryKey("items"),
+        queryFn: () => dashboardService.getStatusCounts(filter),
+      },
+      {
+        queryKey: getQueryKey("issues"),
+        queryFn: () => dashboardService.getFilteredIssues(filter),
+      },
+    ]);
+  };
+
+  const queryResults = useDashboardData(filter);
+  const queryResult0 = queryResults[0];
+  const queryResult1 = queryResults[1];
+  const statusCounts = queryResult0.data as StatusCounts;
+  const filteredIssues = queryResult1.data as FilteredIssues;
+
+  function onMonthRangeTap(months: number) {
+    const range = getDateRange(months);
+    setFilter({
+      userId: filter.userId,
+      dateEnd: range.dateEnd,
+      dateStart: range.dateStart,
+    });
+  }
+
+  function getDateRange(months: number): DateRange {
+    const now = new Date();
+    const start = new Date();
+    start.setMonth(start.getMonth() - months);
+    return {
+      dateStart: start,
+      dateEnd: now,
     };
+  }
 
-    const queryResults = useDashboardData(filter);
-    const queryResult0 = queryResults[0];
-    const queryResult1 = queryResults[1];
-    const statusCounts = queryResult0.data as StatusCounts;
-    const filteredIssues = queryResult1.data as FilteredIssues;
+  function userFilterOpen() {
+    users$.subscribe((users: PtUser[]) => {
+      if (users.length > 0) {
+        setUsers(users);
+      }
+    });
 
+    userService.fetchUsers();
+  }
 
-    function onMonthRangeTap(months: number) {
-        const range = getDateRange(months);
-        setFilter({
-            userId: filter.userId,
-            dateEnd: range.dateEnd,
-            dateStart: range.dateStart
-        });
-    }
-
-    function getDateRange(months: number): DateRange {
-        const now = new Date();
-        const start = new Date();
-        start.setMonth(start.getMonth() - months);
-        return {
-            dateStart: start,
-            dateEnd: now
-        };
-    }
-
-    function userFilterOpen() {
-        users$.subscribe((users: PtUser[]) => {
-            if (users.length > 0) {
-                setUsers(users);
-            }
-        });
-
-        userService.fetchUsers();
-    }
-
-    function filterItemRender(li: any, itemProps: any) {
-        const userItem = itemProps.dataItem as PtUser;
-        const renderedRow = (
-            <div className="row" style={{ marginLeft: 5 }}>
-                <img className="li-avatar rounded mx-auto d-block" src={userItem.avatar} />
-                <span style={{ marginLeft: 5 }}>{userItem.fullName}</span>
-            </div>
-        );
-        return cloneElement(li, li.props, renderedRow);
-    }
-
-    function userFilterValueChange(e: ComboBoxChangeEvent) {
-        const user = e.target.value;
-        if (user) {
-            setFilter({ ...filter, userId: user.id });
-        } else {
-            setFilter({ ...filter, userId: undefined });
-        }
-    }
-
-    if (queryResult0.isLoading || queryResult1.isLoading) {
-        return (
-            <div>
-                Loading...
-            </div>
-        );
-    }
-    
-    if (!statusCounts || !filteredIssues) {
-        return (
-            <div>No data</div>
-        );
-    }
-
-    return (
-        <div className="dashboard">
-
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-
-                <div className="col-md order-md-first text-center text-md-left">
-                    <h2>
-                        <span className="small text-uppercase text-muted d-block">Statistics</span>
-                        {
-                            (filter.dateStart && filter.dateEnd) && (
-                                <span>  {formatDateEnUs(filter.dateStart)} - {formatDateEnUs(filter.dateEnd)}</span>
-                            )
-                        }
-                    </h2>
-                </div>
-
-                <div className="btn-toolbar mb-2 mb-md-0" style={{gap: 20}}>
-
-                    <ComboBox data={users} itemRender={filterItemRender} placeholder={'Select Assignee...'} textField="fullName"
-                                dataItemKey="id" onOpen={userFilterOpen} onChange={userFilterValueChange} style={{ width: 250 }} />
-
-                    <ButtonGroup>
-                        <Button type="button" icon="calendar" onClick={(e) => onMonthRangeTap(3)}>3 Months</Button>
-                        <Button type="button" icon="calendar" onClick={(e) => onMonthRangeTap(6)}>6 Months</Button>
-                        <Button type="button" icon="calendar" onClick={(e) => onMonthRangeTap(12)}>1 Year</Button>
-                    </ButtonGroup>
-                    
-                </div>
-            </div>
-
-            <div className="card">
-                <h3 className="card-header">Active Issues</h3>
-                <div className="card-block">
-
-                    <ActiveIssuesComponent statusCounts={statusCounts}/>
-
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h3>All issues</h3>
-
-                            <DashboardChart issuesAll={filteredIssues} />
-
-                        </div>
-                    </div>
-                </div>
-            </div >
-
-        </div >
+  function filterItemRender(li: any, itemProps: any) {
+    const userItem = itemProps.dataItem as PtUser;
+    const renderedRow = (
+      <div className="row" style={{ marginLeft: 5 }}>
+        <img
+          className="li-avatar rounded mx-auto d-block"
+          src={userItem.avatar}
+        />
+        <span style={{ marginLeft: 5 }}>{userItem.fullName}</span>
+      </div>
     );
+    return cloneElement(li, li.props, renderedRow);
+  }
 
+  function userFilterValueChange(e: ComboBoxChangeEvent) {
+    const user = e.target.value;
+    if (user) {
+      setFilter({ ...filter, userId: user.id });
+    } else {
+      setFilter({ ...filter, userId: undefined });
+    }
+  }
+
+  if (queryResult0.isLoading || queryResult1.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!statusCounts || !filteredIssues) {
+    return <div>No data</div>;
+  }
+
+  return (
+    <div className="dashboard">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+        <div className="col-md order-md-first text-center text-md-left">
+          <h2>
+            <span className="small text-uppercase text-muted d-block">
+              Statistics
+            </span>
+            {filter.dateStart && filter.dateEnd && (
+              <span>
+                {" "}
+                {formatDateEnUs(filter.dateStart)} -{" "}
+                {formatDateEnUs(filter.dateEnd)}
+              </span>
+            )}
+          </h2>
+        </div>
+
+        <div className="btn-toolbar mb-2 mb-md-0" style={{ gap: 20 }}>
+          <ComboBox
+            data={users}
+            itemRender={filterItemRender}
+            placeholder={"Select Assignee..."}
+            textField="fullName"
+            dataItemKey="id"
+            onOpen={userFilterOpen}
+            onChange={userFilterValueChange}
+            style={{ width: 250 }}
+          />
+
+          <ButtonGroup>
+            <Button
+              type="button"
+              icon="calendar"
+              onClick={(e) => onMonthRangeTap(3)}
+            >
+              3 Months
+            </Button>
+            <Button
+              type="button"
+              icon="calendar"
+              onClick={(e) => onMonthRangeTap(6)}
+            >
+              6 Months
+            </Button>
+            <Button
+              type="button"
+              icon="calendar"
+              onClick={(e) => onMonthRangeTap(12)}
+            >
+              1 Year
+            </Button>
+          </ButtonGroup>
+        </div>
+      </div>
+
+      <div className="card">
+        <h3 className="card-header">Active Issues</h3>
+        <div className="card-block pt-2">
+          <ActiveIssuesComponent statusCounts={statusCounts} />
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <h3>All issues</h3>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12">
+              <DashboardChart issuesAll={filteredIssues} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
